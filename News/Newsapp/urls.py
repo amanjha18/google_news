120267ffc0c24d85ab895bf80e26e62809cc9558
amanjha <amanjha813020@gmail.com>
2020-05-15 01:28:21 +0530

    add category_wise dropdown

--- News/Newsapp/urls.py
+++ News/Newsapp/urls.py
@@ -1,8 +1,18 @@
 from django.conf.urls import url
 # from django.contrib import admin
-from .views import index
+from .views import *
 
 urlpatterns = [
-    url(r'^', index, name='index'),
+
+    url('home', home, name='home'),
+    url('world', world,name='world'),
+    url('business', business, name='business'),
+    url('entertainment', entertainment, name='entertainment'),
+    url('health', health,name='health'),
+    url('science', science,name='science'),
+    url('sports', sports, name='sports'),
+    url('technology', technology, name='technology'),
+    url('about', about, name='about'),
+    url('search', search, name='search')
 
 ]
--- News/Newsapp/views.py
+++ News/Newsapp/views.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import requests, json
 from django.shortcuts import render
 from newsapi import NewsApiClient
 # Create your views here.
 
-def index(request):
+def home(request):
 	newsapi = NewsApiClient(api_key='ac3f3a791a17403f911101c1c8893f73')
 	top_headlines = newsapi.get_top_headlines(country='in')
 	articles = top_headlines['articles']
@@ -28,4 +28,202 @@ def index(request):
 	# print(mylist)
 	return render(request, 'index.html', context = {'mylist':mylist})
 
+def world(request):
+	newsapi = NewsApiClient(api_key='ac3f3a791a17403f911101c1c8893f73')
+	all_articles = newsapi.get_everything(sources='bbc-news,the-verge')
+	articles = all_articles['articles']
+	desc = []
+	news = []
+	img = []
+	url = []
+	published =[]
 
+	for i in range(len(articles)):
+		myarticles = articles[i]
+
+		img.append(myarticles['urlToImage'])
+		news.append(myarticles['title'])
+		desc.append(myarticles['description'])
+		url.append(myarticles['url'])
+		published.append(myarticles['publishedAt'])
+	
+	mylist = zip(img, news,  desc, url, published)
+	# print(mylist)
+	return render(request, 'index.html', context = {'mylist':mylist})
+
+
+def business(request):
+	newsapi = NewsApiClient(api_key='ac3f3a791a17403f911101c1c8893f73')
+	all_articles = newsapi.get_top_headlines(country='in', category="business")
+	articles = all_articles['articles']
+	desc = []
+	news = []
+	img = []
+	url = []
+	published =[]
+
+	for i in range(len(articles)):
+		myarticles = articles[i]
+
+		img.append(myarticles['urlToImage'])
+		news.append(myarticles['title'])
+		desc.append(myarticles['description'])
+		url.append(myarticles['url'])
+		published.append(myarticles['publishedAt'])
+
+	mylist = zip(img, news,  desc, url, published)
+	# print(mylist)
+	return render(request, 'index.html', context = {'mylist':mylist})
+
+def entertainment(request):
+	newsapi = NewsApiClient(api_key='ac3f3a791a17403f911101c1c8893f73')
+	all_articles = newsapi.get_top_headlines(country='in', category='entertainment')
+	articles = all_articles['articles']
+	desc = []
+	news = []
+	img = []
+	url = []
+	published =[]
+
+	for i in range(len(articles)):
+		myarticles = articles[i]
+
+		img.append(myarticles['urlToImage'])
+		news.append(myarticles['title'])
+		desc.append(myarticles['description'])
+		url.append(myarticles['url'])
+		published.append(myarticles['publishedAt'])
+
+	mylist = zip(img, news,  desc, url, published)
+	# print(mylist)
+	return render(request, 'index.html', context = {'mylist':mylist})
+
+def health(request):
+	newsapi = NewsApiClient(api_key='ac3f3a791a17403f911101c1c8893f73')
+	all_articles = newsapi.get_top_headlines(country='in', category='health')
+	articles = all_articles['articles']
+	desc = []
+	news = []
+	img = []
+	url = []
+	published =[]
+
+	for i in range(len(articles)):
+		myarticles = articles[i]
+
+		img.append(myarticles['urlToImage'])
+		news.append(myarticles['title'])
+		desc.append(myarticles['description'])
+		url.append(myarticles['url'])
+		published.append(myarticles['publishedAt'])
+
+	mylist = zip(img, news,  desc, url, published)
+	# print(mylist)
+	return render(request, 'index.html', context = {'mylist':mylist})
+
+def science(request):
+	newsapi = NewsApiClient(api_key='ac3f3a791a17403f911101c1c8893f73')
+	all_articles = newsapi.get_top_headlines(country='in', category='science')
+	articles = all_articles['articles']
+	desc = []
+	news = []
+	img = []
+	url = []
+	published =[]
+
+	for i in range(len(articles)):
+		myarticles = articles[i]
+
+		img.append(myarticles['urlToImage'])
+		news.append(myarticles['title'])
+		desc.append(myarticles['description'])
+		url.append(myarticles['url'])
+		published.append(myarticles['publishedAt'])
+
+	mylist = zip(img, news,  desc, url, published)
+	# print(mylist)
+	return render(request, 'index.html', context = {'mylist':mylist})
+
+
+def sports(request):
+	newsapi = NewsApiClient(api_key='ac3f3a791a17403f911101c1c8893f73')
+	all_articles = newsapi.get_top_headlines(country='in', category='sports')
+	articles = all_articles['articles']
+	desc = []
+	news = []
+	img = []
+	url = []
+	published =[]
+
+	for i in range(len(articles)):
+		myarticles = articles[i]
+
+		img.append(myarticles['urlToImage'])
+		news.append(myarticles['title'])
+		desc.append(myarticles['description'])
+		url.append(myarticles['url'])
+		published.append(myarticles['publishedAt'])
+
+	mylist = zip(img, news,  desc, url, published)
+	# print(mylist)
+	return render(request, 'index.html', context = {'mylist':mylist})
+
+def technology(request):
+	newsapi = NewsApiClient(api_key='ac3f3a791a17403f911101c1c8893f73')
+	all_articles = newsapi.get_top_headlines(country='in', category='technology')
+	articles = all_articles['articles']
+	desc = []
+	news = []
+	img = []
+	url = []
+	published =[]
+
+	for i in range(len(articles)):
+		myarticles = articles[i]
+
+		img.append(myarticles['urlToImage'])
+		news.append(myarticles['title'])
+		desc.append(myarticles['description'])
+		url.append(myarticles['url'])
+		published.append(myarticles['publishedAt'])
+
+	mylist = zip(img, news,  desc, url, published)
+	# print(mylist)
+	return render(request, 'index.html', context = {'mylist':mylist})
+
+def about(request):
+	return render(request,'about.html',{})
+
+def search(request):
+	keyword=""
+	if request.method=='POST':
+		keyword=request.POST['keyword']
+	url="https://newsapi.org/v2/everything?q="+keyword+"&apiKey=ac3f3a791a17403f911101c1c8893f73&language=en&sortBy=publishedAt"
+	api_request=requests.get(url)
+	api=json.loads(api_request.content)
+	if api["status"]=="error":
+		return render(request,'index.html',{'KeyError': 'KeyError'})
+	# return render(request,'index.html',{'api':api,'keyword':keyword})
+	articles = api['articles']
+	desc = []
+	news = []
+	img = []
+	url = []
+	published =[]
+	
+	try:
+		for i in range(len(api)):
+			myarticles = articles[i]
+
+			img.append(myarticles['urlToImage'])
+			news.append(myarticles['title'])
+			desc.append(myarticles['description'])
+			url.append(myarticles['url'])
+			published.append(myarticles['publishedAt'])
+	except IndexError:
+ 		pass
+ 	except KeyError:
+ 		pass
+	mylist = zip(img, news,  desc, url, published)
+	# print(mylist)
+	return render(request, 'index.html', {'mylist':mylist, 'keyword': keyword})
