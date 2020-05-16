87a30720938e32c4ebefc641b1f6b246c8240d35
amanjha <amanjha813020@gmail.com>
2020-05-16 16:57:45 +0530

    change static root and debug=false

--- News/News/settings.py
+++ News/News/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w2xd(bn-kaz*e@y3_^j7o-_bu&0pl@6vhz68be3^!8@cx(4u=g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['aman609.pythonanywhere.com']
 # ALLOWED_HOSTS = []
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'https://aman609.pythonanywhere.com/static')
 #managing media
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_URL = '/media/'
